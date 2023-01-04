197ca3e1cf2986b1054df70192b2be88a57e8a61
Max Burrows <maxajburrows@gmail.com>
2023-01-04 14:39:42 +0100

    Complete second javascript exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let repeatString = '';
 
+    if (num < 0) {
+        repeatString = 'ERROR'
+    }
+
+    for (let i = 0; i < num; i++) {
+        repeatString += string;
+    }
+    return repeatString
 };
 
 // Do not edit below this line
