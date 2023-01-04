041fc67f5610130ff3243030b8c004b935c93fad
Max Burrows <maxajburrows@gmail.com>
2023-01-04 15:19:48 +0100

    Complete third javascript exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let lengthString = string.length;
+    let reverseString = ''
 
+    for (let i = (lengthString-1); i >=0; i--) {
+        reverseString += string.charAt(i)
+    }
+    return reverseString
 };
 
 // Do not edit below this line
