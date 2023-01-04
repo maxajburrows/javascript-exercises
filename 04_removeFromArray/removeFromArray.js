1c36aeeb55fffef1f6562e18f83a397a5da56fc5
Max Burrows <maxajburrows@gmail.com>
2023-01-04 17:36:47 +0100

    Complete fourth Javascript exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...remove) {
+    let filteredArray = array;
+    for (let index of remove) {
+        filteredArray = filteredArray.filter((element) => element !== index);
+    }
+    return filteredArray
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
