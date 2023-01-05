09984cb8995d9e5c7150f3d483f9866a9dd017d3
Max Burrows <maxajburrows@gmail.com>
2023-01-05 10:40:10 +0100

    Complete seventh javascript exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempFahrenheit) {
+    let tempCelsius = Math.round((tempFahrenheit-32)*(5/9)*10)/10;
+    return tempCelsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempCelsius) {
+    let tempFahrenheit = Math.round((tempCelsius*(9/5)+32)*10)/10;
+    return tempFahrenheit
 };
 
 // Do not edit below this line
